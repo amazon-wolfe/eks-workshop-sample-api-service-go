523be8795477c2d25c8a34a41842b41cbc8e545a
amazon-wolfe <69652337+amazon-wolfe@users.noreply.github.com>
2020-10-15 23:33:57 -0500

    Update main.go
    Small modification to test code pipeline

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Oh Hi Mark! - the log message")
 	})
 	http.ListenAndServe(":8080", nil)
 }
